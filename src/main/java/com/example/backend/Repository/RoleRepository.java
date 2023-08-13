577629fc89f35663756ae27ff1eafcd6c827399a
sam <a0983131803@gmail.com>
2023-08-13 20:01:40 +0800

    update role Repository

--- src/main/java/com/example/backend/Repository/RoleRepository.java
+++ src/main/java/com/example/backend/Repository/RoleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backend.Entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role,Long>{
-    
+    Role findByName(String string);
 }
