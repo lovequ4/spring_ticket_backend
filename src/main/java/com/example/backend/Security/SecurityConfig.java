11b3e3745b28fbd8ed58ea041031a23001a6b80a
sam <a0983131803@gmail.com>
2023-08-13 18:45:14 +0800

    security config

---
+++ src/main/java/com/example/backend/Security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.backend.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/api/**",
+                "/api/auth/signup",
+                "/api/auth/Login",
+                "/swagger-ui/**",
+                "/v3/api-docs/**"
+                ).permitAll()
+            .anyRequest()
+            .authenticated()
+            );
+
+        return http.build();    
+    }
+}
