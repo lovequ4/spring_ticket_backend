6ca13c20b17eb76b08b9970b08d003c5130bcb5d
sam <a0983131803@gmail.com>
2023-09-01 07:17:36 +0800

    update concert controller

--- src/main/java/com/example/backend/Controller/ConcertController.java
+++ src/main/java/com/example/backend/Controller/ConcertController.java
@@ -1,6 +1,7 @@
 package com.example.backend.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.backend.Dto.ConcertDto;
 import com.example.backend.Entity.Concert;
@@ -17,7 +20,7 @@ import com.example.backend.Services.ConcertService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
-
+@CrossOrigin
 @RestController
 @Tag(name="Concert")
 @RequestMapping("/api/concert")
@@ -29,12 +32,12 @@ public class ConcertController {
         this.concertService = concertService;
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<String>CreateConcert( @RequestBody ConcertDto concertDto){
-        return concertService.CreateConcert(concertDto);
+    @PostMapping(value="create",consumes = "multipart/form-data")
+    public ResponseEntity<String>CreateConcert( ConcertDto concertDto,@RequestParam MultipartFile concertImg){
+        return concertService.CreateConcert(concertDto,concertImg);
     }
 
-    @GetMapping("/get")
+    @GetMapping("")
     public ResponseEntity<?>getConcert(){
         return concertService.getConcert();
     }
